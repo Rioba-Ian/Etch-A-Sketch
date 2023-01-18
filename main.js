73349b593b0155e84ef21e6231dff61ca5012ef1
Rioba-Ian-Felix <riobafelix@gmail.com>
2023-01-18 13:06:35 +0300

    32 by 32 grid sizes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const container = document.getElementById("container")
 
-const GRID_SIZES = 16
+const GRID_SIZES = 32
 
 container.style.height = `${GRID_SIZES}rem`
 container.style.width = `${GRID_SIZES}rem`
 
 
-for (let i = 0; i <= (GRID_SIZES ** 2 + 64 * 4 + 12); i++) {
+for (let i = 0; i <= (GRID_SIZES * 16 + 32 * 8 + 15); i++) {
   const div = document.createElement("div")
 
-  div.style.width = "8px"
-  div.style.height = "8px"
+  div.style.width = "16px"
+  div.style.height = "16px"
 
   div.classList.add("grid")
   container.appendChild(div)
