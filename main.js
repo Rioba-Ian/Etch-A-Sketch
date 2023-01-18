a2239c52e07e313499a3f0cf52b034ad9e29b73b
Rioba-Ian-Felix <riobafelix@gmail.com>
2023-01-18 09:38:53 +0300

    grid squares to fit container

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const container = document.getElementById("container")
 
+const GRID_SIZES = 16
+
+container.style.height = `${GRID_SIZES}rem`
+container.style.width = `${GRID_SIZES}rem`
 
 
-for (let i = 0; i < 40; i++) {
+for (let i = 0; i <= (GRID_SIZES ** 2 + 256 + 64 + 32 + 16); i++) {
   const div = document.createElement("div")
 
-  div.style.width = `${(container.clientWidth / container.clientWidth) * 0.9}rem`
-  div.style.height = `${container.clientHeight / container.clientHeight}rem`
+  div.style.width = "8px"
+  div.style.height = "8px"
 
   div.classList.add("grid")
   container.appendChild(div)
